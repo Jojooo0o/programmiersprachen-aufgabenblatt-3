41ceca2dcce5d0a286c81815cb868cf43cedd7f4
Jonas Dorsch <jesa8605@billl11.in.uni-weimar.de>
2016-05-20 18:28:44 +0200

    3.10 finished

--- source/aufgabe7.cpp
+++ source/aufgabe7.cpp
@@ -91,6 +91,26 @@ TEST_CASE("describe_sort2", "[sort2]"){
 	REQUIRE(std::is_sorted(list0.begin(), list0.end()));
 }
 
+TEST_CASE("describe_add_lambda", "[add_lambda]"){
+
+
+	std::vector<int> v1{1,2,3,4,5,6,7,8,9};
+	std::vector<int> v2{9,8,7,6,5,4,3,2,1};
+	std::vector<int> v3(9);
+
+	std::transform(v1.begin(), v1.end(), v2.begin(), v3.begin(), []( int a, int b) {
+		return a+b;
+	});
+
+	REQUIRE(std::all_of(v3.begin(), v3.end(), [] (int a) {
+		return a == 10;
+	}));
+
+
+
+}
+
+
 
 
 
